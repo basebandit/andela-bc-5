54d18d7fca586093d826704360e31b8b54d572f6
Evanson <evansonmwangi83@gmail.com>
2016-01-26 15:03:39 +0300

    Add a new reversex function

--- str_reverse.py
+++ str_reverse.py
@@ -1,6 +1,17 @@
 def reverse(s):
     return s[::-1]
 
-#examples
+"""Returns the reverse of a string"""
+
+
+def reversex(s):
+    for i in range(len(s) // 2):
+        s = list(s)
+        s[i], s[len(s) - i - 1] = s[len(s) - i - 1], s[i]
+
+    return "".join(s)
+
+
+# examples
 print reverse("mwas")
-print reverse("hello")
+print reversex("hello")
